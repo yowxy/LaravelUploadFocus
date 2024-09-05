2045a6eca7cbcdf41a999b31c320b4904d1fbe1f
yowxy <iklilblender@gmail.com>
2024-09-06 01:32:30 +0700

    Menambahkan data register ke database

--- laravel-basic/app/Http/Controllers/AuthController.php
+++ laravel-basic/app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 class AuthController extends Controller
@@ -43,6 +45,15 @@ class AuthController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6|max:255|confirmed',
         ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return redirect ('login');
+
     }
 
 }
