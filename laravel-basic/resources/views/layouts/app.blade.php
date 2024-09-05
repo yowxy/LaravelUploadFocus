441b4ac1dc0a9f4285e574e6121594c82fabd3be
yowxy <iklilblender@gmail.com>
2024-09-05 13:46:14 +0700

    Implementasi Layout blade Laravel

---
+++ laravel-basic/resources/views/layouts/app.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}  " rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }} "
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
+    <style>
+        .blog {
+            padding: 5px;
+            /* border-bottom: 1px solid lightgrey; */
+            /* font-family: 'Poppins',sans-serif; */
+
+        }
+
+        small {
+            color: gray;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <header class="p-3 text-bg-dark mb-4 ">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                </ul>
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+
+        @yield('content')
+
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            </ul>
+        </footer>
+    </div>
+</html>
--- laravel-basic/resources/views/posts/index.blade.php
+++ laravel-basic/resources/views/posts/index.blade.php
@@ -1,87 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+{{-- bisa pakai @section('title','CRUD Laravel 🚀') soal nya cuman satu line aja  --}}
 
-    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}  " rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }} "
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+@section('title', )
+    CRUD Laravel 🚀
+@endsection
 
-    {{--
-    <link href="bootstrap-5/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="bootstrap-5/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script> --}}
-    <style>
-        .blog {
-            padding: 5px;
-            /* border-bottom: 1px solid lightgrey; */
-            /* font-family: 'Poppins',sans-serif; */
-
-        }
-
-        small {
-            color: gray;
-        }
-    </style>
-
-</head>
-
-<body>
-
-    <header class="p-3 text-bg-dark mb-4 ">
-        <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                </ul>
-                <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2">Login</button>
-                </div>
-            </div>
+@section('content')
+    <h1>
+        Hello World
+        <a class="btn btn-success" href="{{ url('posts/create') }}">+ Buat Postingan </a>
+    </h1>
+    @foreach ($post as $posts)
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">{{ $posts->title }}</h5>
+            <p class="card-text">{{ $posts->content }}</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated
+                    {{ date('d M Y H:i', strtotime($posts->created_at)) }}</small></p>
+            <a href="{{ url("posts/$posts->id") }}" class="btn btn-primary">Selengkapnya</a>
+            <a href="{{ url("posts/$posts->id/edit") }}" class="btn btn-info">Edit</a>
         </div>
-    </header>
-
-    <div class="container">
-        <h1>
-            Hello World
-
-            <a class="btn btn-success" href="{{ url('posts/create') }}">+ Buat Postingan
-            </a>
-
-        </h1>
-        {{-- @php($number = 1) --}}
-        @foreach ($post as $posts)
-
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ $posts->title }}</h5>
-                <p class="card-text">{{ $posts->content }}</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated {{ date("d M Y H:i",
-                        strtotime($posts->created_at)) }}</small></p>
-                <a href="{{ url("posts/$posts->id") }}" class="btn btn-primary">Selengkapnya</a>
-                <a href="{{ url("posts/$posts->id/edit") }}" class="btn btn-info">Edit</a>
-            </div>
-        </div>
-
-        @endforeach
-
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
-
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-                </ul>
-            </footer>
     </div>
-
-</body>
-
-</html>
+    @endforeach
+    </body>
+@endsection
