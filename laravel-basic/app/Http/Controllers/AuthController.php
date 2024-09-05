f33eac99e8a13991ff30b24f6bc16f9dd2c62399
yowxy <iklilblender@gmail.com>
2024-09-05 21:21:43 +0700

    Menambahkan sebuah fitur error massage  Auth Pada form login

--- laravel-basic/app/Http/Controllers/AuthController.php
+++ laravel-basic/app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 class AuthController extends Controller
@@ -15,8 +16,13 @@ class AuthController extends Controller
         return view('auth.login'); // Ensure the view exists at resources/views/auth/login.blade.php
     }
 
-    public function authenticate(){
-
+    public function authenticate(Request $request){
+        $credentials = $request->only('email','password');
+        if(Auth::attempt($credentials)){
+            return redirect('posts');
+        }else{
+            return redirect('login')->with('error_message','Failed To Login please check email or password');
+        }
     }
 
 }
--- laravel-basic/resources/views/auth/login.blade.php
+++ laravel-basic/resources/views/auth/login.blade.php
@@ -10,21 +10,28 @@
         <div class="card-body">
             <h1 class="text-center">Login</h1>
 
-            <form method="POST" action="{{url('login')}}" class="form-control">
-                @csrf
+            @if (session()->has('error_message'))
+            <div class="alert alert-danger">
+                {{ session()->get('error_message') }}
+            </div>
+        @endif
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password">
-                </div>
-                <div class="mb-3">
-                    <a href="{{url('login')}}" type="submit" class="btn btn-primary">Log In</a>
-                </div>
-            </form>
+        <form method="POST" action="{{ url('login') }}" class="form-control">
+            @csrf
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Log In</button>
+            </div>
+        </form>
+
         </div>
     </div>
 </div>
