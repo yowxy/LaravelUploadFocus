68b0eba9718c4ff9b782767bcd916cb799561f26
yowxy <iklilblender@gmail.com>
2024-09-07 11:41:02 +0700

    Membuat sebuah Migrations lstings dan Model Listig

---
+++ RentHouse/app/Models/Listing.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Listing extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'address',
+        'sqft',
+        'wifi_speed',
+        'max_person',
+        'price_per_day',
+        'attachments',
+        'full_support_available',
+        'gym_area_available',
+        'cinema_available'
+    ];
+
+    protected $cast = [
+        'attachments' => 'array',
+    ];
+
+}
