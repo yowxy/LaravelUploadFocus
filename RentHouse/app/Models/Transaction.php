7c2f848169c8d5e5273c3b9d03ca70e04a90d579
yowxy <iklilblender@gmail.com>
2024-09-09 07:07:26 +0700

    Membuat model dan migration Transaction

---
+++ RentHouse/app/Models/Transaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'listing_id',
+        'start_date',
+        'end_date',
+        'price_per_day',
+        'total_days',
+        'fee',
+        'total_price',
+        'status'
+    ];
+
+    public function setListingidAttribute($value){
+        $listing = Listing::find($value);
+        $total_days = Carbon::createFromDate($this->attributes['start_date'])->diffInDays($this->attributes['end_date']+1);
+        $totalPrice = $listing->price_per_day * $total_days;
+        $fee = $totalPrice * 0.1;
+
+        $this->attributes['listing_id'] = $value;
+        $this->attributes['price_per_day'] =   $listing->price_per_day;
+        $this->attributes['tota_days'] = $total_days;
+        $this->attributes['fee'] = $fee;
+        $this->attributes['total_price'] = $totalPrice * $fee;
+
+
+    }
+}
