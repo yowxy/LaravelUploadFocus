6de2715a1bf5053fb12ef90a6c47485c97149220
yowxy <iklilblender@gmail.com>
2024-09-10 04:34:32 +0700

    Membuat sebuah tampiln dashboard dan menginstal library laravel-trend untuk chart

--- RentHouse/app/Filament/Widgets/StatsOverview.php
+++ RentHouse/app/Filament/Widgets/StatsOverview.php
@@ -2,17 +2,48 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Listing;
+use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
+    private function getPercentage(int $from, int $to)
+    {
+        // Calculate the denominator
+        $denominator = $to + ($from / 2);
+
+        // Check if the denominator is zero to avoid division by zero
+        if ($denominator == 0) {
+            return 0;  // Return 0 or another fallback value to avoid division by zero
+        }
+
+        // Calculate the percentage change
+        return (($to - $from) / $denominator) * 100;
+    }
+
+
     protected function getStats(): array
     {
+        $newListing = Listing::whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)->count();
+        $transactions = Transaction::whereStatus('approved')->whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year);
+        $prevTransactions = Transaction::whereStatus('approved')->whereMonth('created_at', Carbon::now()->subMonth()->month)->whereYear('created_at', Carbon::now()->subMonth()->year);
+        $transactionPercentage = $this->getPercentage($prevTransactions->count(), $transactions->count());
+        $revenuePercentage = $this->getPercentage($prevTransactions->sum('total_price'), $transactions->sum('total_price'));
+
         return [
-            Stat::make('Unique views', '192.1k'),
-            Stat::make('Bounce rate', '21%'),
-            Stat::make('Average time on page', '3:12'),
+            Stat::make('New listing of the month', $newListing),
+            Stat::make('Transaction of the month', $transactions->count())
+            ->description($transactionPercentage > 0 ? "{$transactionPercentage}% increased" : "{$transactionPercentage}% decreased")
+            ->descriptionIcon($transactionPercentage > 0 ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down')
+            ->color($transactionPercentage > 0 ? 'success' : 'danger'),
+            Stat::make('Revenue of the month', \Number::currency($transactions->sum('total_price'), 'USD'))
+                ->description($revenuePercentage > 0 ? "{$revenuePercentage}% increased" : "{$revenuePercentage}% decreased")
+                ->descriptionIcon($revenuePercentage > 0 ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down')
+                ->color($revenuePercentage > 0 ? 'success' : 'danger'),
         ];
     }
 }
