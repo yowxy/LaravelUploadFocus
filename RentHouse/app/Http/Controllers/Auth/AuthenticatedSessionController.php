65c3cc3c3a79b6f9da4be90ee47db4f65fb66050
yowxy <iklilblender@gmail.com>
2024-09-10 14:35:15 +0700

    Membuat endpoint login dan Mengenerate sebuah Token baru

--- RentHouse/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ RentHouse/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,20 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user = Auth::user();
+        $user ['token'] = $request->user()->createToken('auth')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Successfully',
+            'data' => $user,
+        ]);
     }
 
     /**
--- RentHouse/bootstrap/app.php
+++ RentHouse/bootstrap/app.php
@@ -14,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+        ]);
+
+        $middleware->validateCsrfTokens(except:[
+            '*'
         ]);
 
         $middleware->alias([
