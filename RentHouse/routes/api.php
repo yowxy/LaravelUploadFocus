f28fd686538e1fdee28cf9335b52754e3023051a
yowxy <iklilblender@gmail.com>
2024-09-10 15:16:41 +0700

    Membuat endpoint me Auth

--- RentHouse/routes/api.php
+++ RentHouse/routes/api.php
@@ -4,7 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json([
+        'success' =>true,
+        'message' => 'login successfully',
+        'data' => $request->user(),
+    ]);
 });
 
 require __DIR__.'/auth.php';
