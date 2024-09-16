f55ed777cae7503dcf290bc8ee5871bb4a9ab98b
yowxy <iklilblender@gmail.com>
2024-09-17 06:58:24 +0700

    Menmabahkan NextAuth ENV dan Session Provider

--- RentHouse-Frontend/src/providers/redux.tsx
+++ RentHouse-Frontend/src/providers/redux.tsx
@@ -1,11 +1,16 @@
 "use client"
+import {SessionProvider} from "next-auth/react";
 import { store } from "@/lib/store";
 import { Provider } from "react-redux";
 
 function ReduxProvider({children}: {children :React.ReactNode} ){
-     return <Provider store= {store} >
-        {children}
-     </Provider>;
+     return 
+     <SessionProvider>
+         <Provider store= {store} >
+            {children}
+         </Provider>;
+     </SessionProvider>
+     
 } 
 
 export default ReduxProvider;
