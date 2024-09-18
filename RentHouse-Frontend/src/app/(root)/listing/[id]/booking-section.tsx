c26ead246bf7350f34d6ed0fbe8dc75b32ee0819
yowxy <iklilblender@gmail.com>
2024-09-18 15:07:00 +0700

    Membuat sebuah start date , end date dan integrasi booking price

--- RentHouse-Frontend/src/app/(root)/listing/[id]/booking-section.tsx
+++ RentHouse-Frontend/src/app/(root)/listing/[id]/booking-section.tsx
@@ -3,9 +3,10 @@ import Title from "@/components/atomics/title";
 import CardBooking from "@/components/molecules/card/card-booking";
 import { DatePickerDemo } from "@/components/molecules/date-picker";
 import { moneyFormat } from "@/lib/utils";
+import moment from "moment";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 interface BookingSectionProps {
   id: string;
@@ -18,6 +19,23 @@ function BookingSection({ id , price}: BookingSectionProps) {
 
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
+
+  const booking = useMemo(() => {
+    let totalDays = 0,
+        subtotal =  0,
+        tax = 0,
+        grandTotal = 0;
+
+    if(startDate && endDate){
+      totalDays = moment(endDate).diff(startDate, "days");
+      subtotal = totalDays * price;
+      tax = subtotal * 0.1;
+      grandTotal = subtotal + tax;
+    }
+    
+    return {totalDays, subtotal, tax, grandTotal}
+
+  },[startDate, endDate])
 
   return (
     <div className="w-full max-w-[360px] xl:max-w-[400px] h-fit space-y-5 bg-white border border-border rounded-[20px] p-[30px] shadow-indicator">
@@ -41,11 +59,10 @@ function BookingSection({ id , price}: BookingSectionProps) {
         />
       </div>
       <div className="space-y-5">
-        <CardBooking title="Total days" value="30 days" />
-        <CardBooking title="Sub total" value="$83,422" />
-        <CardBooking title="Tax (10%)" value="$23,399" />
-        <CardBooking title="Insurance" value="$7,492" />
-        <CardBooking title="Grand total price" value="$103,940" />
+        <CardBooking title="Total days" value={`${booking.totalDays}`} />
+        <CardBooking title="Sub total" value={moneyFormat.format(booking.subtotal)}/>
+        <CardBooking title="Tax (10%)" value={moneyFormat.format(booking.tax)} />
+        <CardBooking title="Grand total price" value={moneyFormat.format(booking.grandTotal)} />
       </div>
       <Link href={`/listing/${id}/checkout`}>
         <Button variant="default" className="mt-4">
