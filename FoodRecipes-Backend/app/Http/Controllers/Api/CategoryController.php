704b21530eec641371361fa1cb6e81ca006c7788
yowxy <iklilblender@gmail.com>
2024-09-22 15:24:59 +0700

    Create API and Testing API Category

--- FoodRecipes-Backend/app/Http/Controllers/Api/CategoryController.php
+++ FoodRecipes-Backend/app/Http/Controllers/Api/CategoryController.php
@@ -12,10 +12,11 @@ class CategoryController extends Controller
     public function index(){
         $categories = Category::withCount('recipes')->get();
         return CategoryResource::collection($categories);
-
     }
+
     public function show (Category $category){
         $category->load('recipes');
+        $category->loadCount('recipes');
         return new CategoryResource($category);
     }
 }
