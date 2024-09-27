eca40655607aca034291720d67a86275e8aa5316
yowxy <iklilblender@gmail.com>
2024-09-27 14:56:43 +0700

    Create a register

---
+++ MineFolioX/resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+{{-- Footer --}}
+@vite(['resources/scss/index.scss','resources/app/js'])
+<footer id="footer-last">
+    <div class="container">
+        <h1 class="text-white pt-5  mx-auto">MineFolioX</h1>
+        <div class="mx-1">
+            <img src="{{ url('images/instagram.png') }}" alt="instagram" class="me-2 ">
+            <img src="{{ url('images/facebook.png') }}" alt="facebook" class="me-2" >
+            <img src="{{ url('images/X.png') }}" alt="X" class="me-2" >
+        </div>
+    </div>
+</footer>
--- MineFolioX/resources/views/pages/register.blade.php
+++ MineFolioX/resources/views/pages/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +9,44 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
     @vite(['resources/scss/index.scss','resources/app/js'])
 </head>
+
 <body>
-    <h1>Hello world</h1>
+    <nav id="MineFolioX">
+        <a class="navbar-brand" href="{{ url('/') }}">MineFolioX</a>
+    </nav>
+
+    <div class="MineFolioX-input">
+        <img src="{{ url('images/work11.png') }}" alt="" class="img-work">
+        <div class="MineFolioX-input-bawah">
+            <div style="width: 463px; height: 118px; left: 0px; top: 131px; position: absolute">
+                <p class="register-name-email" >Email</p>
+                <input type="email"  class="register-input-email" placeholder="Pastikan email telah di inputkan" ></input>
+            </div>
+            <div style="width: 463px; height: 118px; left: 0px; top: 0px; position: absolute">
+                <p class="register-name" >Name</p>
+                <input class="register-input-name" type="text" placeholder="Pastikan nama nya telah di isi" ></input>
+            </div>
+            <div style="width: 463px; height: 128px; left: 0px; top: 262px; position: absolute">
+                <p class="register-password" >Password</p>
+                <input type="password" class="register-input-password" placeholder="Pastikan password nya telah di isi " ></input>
+            </div>
+            <div style="width: 463px; height: 128px; left: 0px; top: 403px; position: absolute">
+                <p class="register-confirm-password">Confirm Password</p>
+                <input class="register-input-pw" type="password" placeholder="Pastikan password yang di input sama " ></input>
+            </div>
+            <div style="width: 463px; height: 73px; left: 0px; top: 590px; position: absolute">
+                <button  class="btn-register  ">Register</button>
+            </div>
+        </div>
+        <div style="width: 329px; height: 122px; left: 798px; top: 0px; position: absolute">
+            <h1 class="txt-MineFolioX">MineFolioX</h1>
+            <p class="txt-paragraf">The best Platfrom can you share <br />your portofolio or any design </p>
+        </div>
+    </div>
+
 </body>
+
 </html>
