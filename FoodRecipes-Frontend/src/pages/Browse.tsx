1cdbf295013a713f24bc48226ad32e456ac2826c
yowxy <iklilblender@gmail.com>
2024-09-29 23:42:20 +0700

    install axios and create CategoryWrapper

--- FoodRecipes-Frontend/src/pages/Browse.tsx
+++ FoodRecipes-Frontend/src/pages/Browse.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
-import CategoryCard from "../components/CategoryCard";
 import FeaturedRecipeCard from "../components/FeaturedRecipeCard";
+import CategoryWrapper from "../wrappers/CategoryWrapper";
 
 export default function Browse() {
   return (
@@ -42,37 +42,7 @@ export default function Browse() {
           </button>
         </form>
       </div>
-      <section id="Categories" className="mt-[30px]">
-        <div className="flex items-center justify-between px-5">
-          <h2 className="font-bold">By Categories</h2>
-        </div>
-        <div className="swiper w-full mt-3">
-          <Swiper
-            className="w-full mt-3"
-            direction="horizontal"
-            spaceBetween={16}
-            slidesPerView="auto"
-            slidesOffsetBefore={20}
-            slidesOffsetAfter={20}
-          >
-            <SwiperSlide className="!w-fit pb-[30px]">
-              <CategoryCard />
-            </SwiperSlide>
-            <SwiperSlide className="!w-fit pb-[30px]">
-              <CategoryCard />
-            </SwiperSlide>
-            <SwiperSlide className="!w-fit pb-[30px]">
-              <CategoryCard />
-            </SwiperSlide>
-            <SwiperSlide className="!w-fit pb-[30px]">
-              <CategoryCard />
-            </SwiperSlide>
-            <SwiperSlide className="!w-fit pb-[30px]">
-              <CategoryCard />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </section>
+      <CategoryWrapper></CategoryWrapper>
       <section id="MadeByPeople">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-bold">Made by People</h2>
---
+++ FoodRecipes-Frontend/src/wrappers/CategoryWrapper.tsx
@@ -0,0 +1,68 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import CategoryCard from "../components/CategoryCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function CategoryWrapper() {
+
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/categories')
+            .then(response => {
+                setCategories(response.data.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    },[]);
+
+    if(loading){
+        return <p>Loading...</p>
+    }
+
+    if(error){
+        return <p>Error loading data: {error}</p>
+    }
+
+    return (
+        <>
+                 <section id="Categories" className="mt-[30px]">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-bold">By Categories</h2>
+        </div>
+        <div className="swiper w-full mt-3">
+          <Swiper
+            className="w-full mt-3"
+            direction="horizontal"
+            spaceBetween={16}
+            slidesPerView="auto"
+            slidesOffsetBefore={20}
+            slidesOffsetAfter={20}
+          >
+            <SwiperSlide className="!w-fit pb-[30px]">
+              <CategoryCard />
+            </SwiperSlide>
+            <SwiperSlide className="!w-fit pb-[30px]">
+              <CategoryCard />
+            </SwiperSlide>
+            <SwiperSlide className="!w-fit pb-[30px]">
+              <CategoryCard />
+            </SwiperSlide>
+            <SwiperSlide className="!w-fit pb-[30px]">
+              <CategoryCard />
+            </SwiperSlide>
+            <SwiperSlide className="!w-fit pb-[30px]">
+              <CategoryCard />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+        </>
+    );
+}
