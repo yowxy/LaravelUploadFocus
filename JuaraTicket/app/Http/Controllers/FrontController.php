25f011ab8dada3b1b7037c8003f8a063b41a6bfc
yowxy <iklilblender@gmail.com>
2024-10-22 15:44:59 +0700

    start a frontend with a service repository pattern

--- JuaraTicket/app/Http/Controllers/FrontController.php
+++ JuaraTicket/app/Http/Controllers/FrontController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    protected $frontService;
+
+
+
+    // Konsep Service Pattern
+
+    public function  __construct(FrontService $frontService){
+        $this->frontService = $frontService;
+    }
+
+
+    public function index (){
+        $data = $this->frontService->getFrontPageData();
+        return view('front.index', $data);
+    }
+
+    // Konsep MVC
+
+    // public function index(){
+    //     $categories = Category::latest()->get();
+    //     $popular_tickets = Ticket::where('is_popular', true)->take(4)->get();
+    //     $new_tickets = Ticket::latest()->get();
+    //     return view('front,.index', compact('categories','popular_tickets','new_tickets'));
+    // }
 }
