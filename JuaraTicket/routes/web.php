96a885e85005da87168617e05d8e7520b86c8f9a
yowxy <iklilblender@gmail.com>
2024-10-22 12:15:29 +0700

    create Routing for interface Frontend

--- JuaraTicket/routes/web.php
+++ JuaraTicket/routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Filament\Resources\BookingTransactionResource\Pages\CreateBookingTransaction;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+Route::get('/', [FrontController::class , 'index'])->name('front.index');
+
+Route::get('/browse/{category:slug}', [FrontController::class , 'category'])->name('front.category');
+
+
+Route::get('/detail/{ticket:slug}'  ,[FrontController::class  , 'details'])->name('front.details');
+
+Route::get('/check-booking', [ BookingController::class ,'checkBooking'])->name('front.check.booking');
+Route::post('/check-booking/details', [BookingController::class])->name('front.check_booking_details');
+
+
+Route::get('/booking/payment', [BookingController::class , 'payment'])->name('front.payment');
+Route::post('/booking/payment', [BookingController::class , 'paymentStore'])->name('front.payment_store');
+
+Route::get('/booking/{ticket:slug}', [BookingController::class , 'booking'])->name('front.booking');
+Route::post('/booking/{ticket:slug}', [BookingController::class , 'bookingStore'])->name('front.booking.store');
+
+Route::get('/booking/finished/{bookingTransaction}', [BookingController::class ,'BookingFinished'])->name('front.booking_finished');
