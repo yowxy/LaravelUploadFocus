9693593dc2c400138ac4359a0a23bc8ca436a1b7
yowxy <iklilblender@gmail.com>
2024-10-30 09:49:56 +0700

    create  BookingController

--- JuaraTicket/app/Http/Controllers/BookingController.php
+++ JuaraTicket/app/Http/Controllers/BookingController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBookingRequest;
+use App\Models\Ticket;
+use App\Services\BookingService;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    protected $bookingService;
+
+    public function __construct(BookingService $bookingService){
+        $this->bookingService = $bookingService;
+    }
+
+    public function booking (Ticket $ticket)
+    {
+        dd($ticket);
+        // return view('front.booking', compact('ticket'));
+    }
+
+    public function bookingStore(Ticket $ticket, StoreBookingRequest $request)
+    {
+        $validated = $request->$validated();
+
+        $totals = $this->bookingService->calculateTotals($ticket->id, $validated['total_participant']);
+        $this->bookingService->storeBookingInSession($ticket , $validated , $totals);
+
+        // return redirect()->route('front.payment');
+    }
 }
